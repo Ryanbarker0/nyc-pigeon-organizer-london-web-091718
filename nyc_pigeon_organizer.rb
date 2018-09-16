e58518f71f68ea5f9d83972b3aaec6de619e2eda
Ryan Barker <ryangeorgebarker0@gmail.com>
2018-09-16 15:31:07 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -6,10 +6,10 @@ def nyc_pigeon_organizer(data)
     char.each do |att, name|
       name.each do |names|
         if new_hash.has_key?(names) == false
-          new_hash[names] = {}
+          new_hash[names] = Hash.new
         end
         if new_hash[names].has_key?(detail) == false
-          new_hash[names][detail] = []
+          new_hash[names][detail] = Array.new
         end
           new_hash[names][detail] << att.t_s
         end
